aaea9eb942022c137d029db6a995d566df7ab965
DutchRascal <DutchRascal@fritz.box>
2020-07-25 15:05:07 +0200

    Init

---
+++ Assets/Math003.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class Math003 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Floor: " + Mathf.Floor(6.283f));
+        print("Ceil: " + Mathf.Ceil(6.283f));
+        print("Round 2: " + System.Math.Round(6.283f, 2));
+        print("Round 1: " + System.Math.Round(6.283f, 1));
+        print("Power: "+ Mathf.Pow(2,24));
+    }
+}
